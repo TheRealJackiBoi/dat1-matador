47b748b54fe4411cca60b27b2ab3e75960cedbec
TheRealJackiBoi <jslam@oulund.dk>
2022-09-11 21:48:44 +0200

    update

--- Main.java
+++ Main.java
@@ -1,12 +1,43 @@
-public class Main {
+import java.util.ArrayList;
+import java.util.Scanner;
 
-    Player player1 = new Player("Jack");
+public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(player1);
+        ArrayList<Player> players = createPlayers();
+
+        Integer tiles = 40;
+
+        String[] propertyNames = {"Rødovrevej", "Hvidovrevej", "Scandlines", "Roskildevej", "Valby Langgade", "Allégade", "Frederiksberg Allé", "Squash", "Bülowsvej", "Gl. Kongevej", "Mols-Linjen", "Bernstorffsvej", "Hellerupvej", "Strandvejen", "Parkering", "Trianglen", "Østerbrogade", "Grønningen", "Scandlines2", "Bredgade", "Kgs. Nytorv", "Coca Cola", "Østergade", "Amagertorv", "Vimmelskaftet", "Nygade", "Scandlines3", "Frederiksberggade", "Rådhuspladsen"};
+
+        ArrayList<Property> properties = new ArrayList<>();
+
+        for (Player player:
+             players) {
+            player.displayStats();
+            System.out.println("");
+        }
 
 
+    }
+
+    public static ArrayList<Player> createPlayers() {
+        ArrayList<Player> players = new ArrayList<>(4);
+
+        Scanner playerScanner = new Scanner(System.in);
+
+        for (int i = 0; i < 4; i++) {
+
+            System.out.println("What is the name of your player? ");
+            String pName = playerScanner.nextLine();
+
+            Player p = new Player( pName );
+
+            players.add(p);
+        }
+
+        return players;
     }
 
 }
--- Player.java
+++ Player.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
+
 public class Player {
 
     private int money = 0;
-    private Property[] properties = {};
+    private ArrayList<Property> properties = new ArrayList<>();
     private int position = 0;
+    private int lap = 0;
     private String name = "";
 
     public Player(String setName) {
@@ -10,21 +13,48 @@ public class Player {
         name = setName;
         money = 30000;
 
-        println("New player: " + name);
-        println("Money: " + money);
+        System.out.println("New player: " + name);
+        System.out.println("Money: " + money);
 
     }
 
     public void displayStats() {
-        println("Player " + name + " STATS:");
-        println("Money: " + money);
-        println("Position: ?" + position);
+        System.out.println("Player " + name + " STATS:");
+        System.out.println("Money: " + money);
+        System.out.println("Position: ?" + position + ", lap " + lap);
+        if ( properties.size() > 0) {
+            System.out.println("Properties: ");
+            for (Property prop : properties) {
+                System.out.println(prop);
+            }
+        } else System.out.println(name  + " owns no properties");
+        System.out.println("");
     }
 
     public void buyProperty(Property newProperty) {
+        money -= newProperty.getPrice();
 
-
+        properties.add(newProperty);
     }
 
 
+    public int getMoney() {
+        return money;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public int getLap() {
+        return lap;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Property> getProperties() {
+        return properties;
+    }
 }
